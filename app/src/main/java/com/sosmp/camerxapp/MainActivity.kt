c0bf57dee546c39dd0f6cd1ba3f23c7c71d25b20
tonyfarag <tony@faredgestudio.com>
2021-08-27 13:26:13 -0700

    More logginig

--- app/src/main/java/com/sosmp/camerxapp/MainActivity.kt
+++ app/src/main/java/com/sosmp/camerxapp/MainActivity.kt
@@ -107,7 +107,7 @@ class MainActivity : AppCompatActivity() {
     private fun processImage(inputImage: InputImage) {
         val options = BarcodeScannerOptions.Builder()
             .setBarcodeFormats(
-                Barcode.FORMAT_PDF417
+                Barcode.FORMAT_ALL_FORMATS
             )
             .build()
         val scanner = BarcodeScanning.getClient(options)
@@ -117,11 +117,15 @@ class MainActivity : AppCompatActivity() {
                 // Task completed successfully
                 // ...
                 Log.i("FOXX SUCCESS", "successfullly scanned");
+
+                for (barcode in barcodes) {
+                    Log.i("FOXX SUCCESS", "     received barcode: " + barcode.rawValue);
+                }
             }
             .addOnFailureListener {
                 // Task failed with an exception
                 // ...
-                Log.i("FOXX FAILURE", "failed to scan");
+                Log.i("FES FAILURE", "failed to scan with error: " +  it.localizedMessage);
             }
     }
 
